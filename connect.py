053b5763d87813206310585d8a0c61da5c3be399
Hao Wang <wh690847460@gmail.com>
2023-10-11 09:49:20 +0000

    milestone

---
+++ connect.py
@@ -0,0 +1,7 @@
+# These details are available on the first MySQL Workbench screen
+# Usually called 'Local Instance'
+dbuser = "dummyboy" # Your MySQL username - likely 'root'
+dbpass = "assignment" # ---- PUT YOUR PASSWORD HERE ----
+dbhost = "dummyboy.mysql.pythonanywhere-services.com" 
+dbport = "3306"
+dbname = "dummyboy$motorkhana"
